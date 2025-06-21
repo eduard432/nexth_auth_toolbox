ce3614a83698b1cf0a7f0e5889d9119de169fe51
eduard432 <juantokx@gmail.com>
2025-06-21 16:15:29 -0600

    Create User

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -2,6 +2,9 @@
 
 import { RegisterSchema } from '@/schemas'
 import z from 'zod'
+import bcryptjs from 'bcryptjs'
+import { db } from '@/lib/db'
+import { getUserByEmail } from '@/data/user'
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values)
@@ -12,5 +15,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         }
     }
 
-    return { success: "Email sent!" }
+    const { email, password, name } = validatedFields.data
+    const hashedPassword = await bcryptjs.hash(password, 10)
+
+    const existingUser = await getUserByEmail(email)
+
+    if(existingUser) {
+        return { error: "Email alredy in use!" }
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    })
+
+    // TODO: Send verification token email;
+
+    return { success: "User created" }
 }
